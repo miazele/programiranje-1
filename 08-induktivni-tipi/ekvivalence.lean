1400167de05d2490b8a3169be6b06419657a553a
Mia Zele <mia.zelemia@gmail.com>
2025-12-16 11:43:02 +0000

    update

--- 08-induktivni-tipi/ekvivalence.lean
+++ 08-induktivni-tipi/ekvivalence.lean
@@ -36,7 +36,13 @@ theorem vsota_pomozna : forall (xs : List Int) (acc : Int),
 
 theorem vsoti_enaki : ∀ xs : List Int, vsota xs = vsota' xs :=
   by
-    sorry
+    intro xs
+    calc
+      vsota xs
+      _ = 0 + vsota xs := by rw [Int.zero_add]
+      _ = pomozna xs 0 := by simp [vsota_pomozna]
+      _ = vsota' xs := by simp [vsota']
+
 
 -- 1.b. Seznam poljubnega tipa (List A), ogledamo operacije stika, obračanja in dolžine
 
@@ -68,57 +74,125 @@ def dolzina {A : Type} : List A → Nat :=
 -- Trditve
 theorem trd1  {A : Type} {x : A} : obrni [x] = [x] :=
   by
-    sorry
+    -- rw [obrni, obrni, stakni]
+    simp [obrni, stakni]
+
 
 -- Trditvi 2 in 3 ste na predavanjih dokazali s pomočjo računanja po korakih `calc`
 theorem trd2 {A : Type} {xs ys : List A} : dolzina (stakni xs ys) = dolzina xs + dolzina ys :=
   by
-    sorry
+    induction xs with
+    | nil =>
+        simp [stakni, dolzina]
+    | cons x xs' ih =>
+        simp [stakni, dolzina]
+        rw [ih, Nat.add_assoc]
+
 
 theorem trd3 {A : Type} {xs : List A} : stakni xs [] = xs :=
   by
-    sorry
+    induction xs with
+    | nil =>
+        simp [stakni]
+    | cons x xs' ih =>
+        simp [stakni]
+        rw [ih]
+
 
 theorem trd4 {A : Type} {xs ys zs : List A} : stakni (stakni xs ys) zs = stakni xs (stakni ys zs) :=
   by
-    sorry
+    induction xs with
+    | nil =>
+        simp [stakni]
+    | cons x xs ih =>
+        simp [stakni]
+        rw [ih]
 
 theorem trd5 {A : Type} {xs ys : List A} : obrni (stakni xs ys) = stakni (obrni ys) (obrni xs) :=
   by
-    sorry
+    induction xs with
+    | nil =>
+        simp [stakni, obrni, trd3]
+    | cons x xs' ih =>
+        simp [obrni, stakni]
+        rw [ih]
+        simp [trd4]
+
 
 theorem trd6 {A : Type} {xs : List A} : dolzina (obrni xs) = dolzina xs :=
   by
-    sorry
+    induction xs with
+    | nil =>
+        simp [obrni]
+    | cons x xs' ih =>
+        simp [dolzina, obrni, trd2]
+        rw [ih]
+        omega
+
 
 
 theorem trd7 {A : Type} {xs : List A} : obrni (obrni xs) = xs :=
   by
-    sorry
+    induction xs with
+    | nil =>
+        simp [obrni]
+    | cons x xs' ih =>
+        simp [obrni, trd5, stakni]
+        rw [ih]
 
 -- 1.c. Seznam poljubnega tipa (List A), dodamo preslikave
 
 -- Definicija preslikave
 def preslikaj {A B : Type} : (A → B) → List A → List B :=
   fun f xs =>
-    sorry
+    match xs with
+    | [] => []
+    | x :: xs => f x :: preslikaj f xs
+
 
 -- Trditve
 theorem trd8 {A B C : Type} {f : A → B} {g : B → C} {xs : List A} : preslikaj g (preslikaj f xs) = preslikaj (g ∘ f) xs :=
   by
-    sorry
+    induction xs with
+    | nil =>
+        simp [preslikaj]
+    | cons x xs' ih =>
+        simp [preslikaj]
+        rw [ih]
+
 
 theorem trd9 {A : Type} {xs : List A} : preslikaj id xs = xs :=
   by
-    sorry
+    induction xs with
+    | nil =>
+        simp [preslikaj]
+    | cons x xs ih =>
+        simp [preslikaj]
+        rw [ih]
+
 
 theorem trd10 {A B : Type} {f : A → B} {xs ys : List A} : preslikaj f (stakni xs ys) = stakni (preslikaj f xs) (preslikaj f ys) :=
   by
-    sorry
+    induction xs with
+    | nil =>
+        simp [preslikaj, stakni]
+
+    | cons x xs' ih =>
+        simp [preslikaj, stakni]
+        rw [ih]
+
 
 theorem trd11 {A B : Type} {f : A → B} {xs : List A} : preslikaj f (obrni xs) = obrni (preslikaj f xs) :=
   by
-    sorry
+    induction xs with
+    | nil =>
+      simp [obrni, preslikaj]
+    | cons x xs' ih =>
+      simp [obrni, preslikaj]
+      symm at ih
+      rw [ih]
+      simp [trd10, preslikaj]
+
 
 -- 2. Dvojiška drevesa
 inductive tree (A : Type) : Type where
@@ -137,20 +211,32 @@ def preslikaj_drevo {A B : Type} : (A → B) → tree A → tree B :=
 -- Trditvi
 theorem trd12 {A B : Type} {f : A → B} : preslikaj_drevo f tree.empty = tree.empty :=
   by
-    sorry
+    simp [preslikaj_drevo]
 
 theorem trd13 {A B C : Type} {f : A → B} {g : B → C} {t : tree A} : preslikaj_drevo g (preslikaj_drevo f t) = preslikaj_drevo (g ∘ f) t :=
   by
-    sorry
+    induction t with
+    | empty =>
+        simp [preslikaj_drevo]
+    | node x l r ihl ihr =>
+        simp [preslikaj_drevo]
+        -- exact ⟨ihl, ihr⟩
+        constructor
+        · exact ihl
+        · exact ihr
 
 -- 2.b. Globina drevesa in zrcaljenje drevesa
 def globina {A : Type} : tree A → Nat :=
   fun t =>
-    sorry
+    match t with
+    | .empty => 0
+    | .node _ l r => 1 + Nat.max (globina l) (globina r)
 
 def zrcali {A : Type} : tree A → tree A :=
   fun t =>
-    sorry
+    match t with
+    | .empty => .empty
+    | .node x l r => .node x (zrcali r) (zrcali l)
 
 theorem max_comm {a b : Nat} : Nat.max a b = Nat.max b a := -- To trditev preberemo iz knjižnice
   Nat.max_comm a b
@@ -158,36 +244,81 @@ theorem max_comm {a b : Nat} : Nat.max a b = Nat.max b a := -- To trditev preber
 -- Trditvi
 theorem trd14 {A : Type} {t : tree A} : globina (zrcali t) = globina t :=
   by
-    sorry
+    induction t with
+    | empty =>
+        simp [zrcali, globina]
+    | node x l r ihr ihl =>
+        simp [globina, zrcali]
+        rw [ihl, ihr]
+        simp [max_comm]
 
 theorem trd15 {A : Type} {t : tree A} : zrcali (zrcali t) = t :=
   by
-    sorry
+    induction t with
+    | empty =>
+        simp [zrcali]
+    | node x l r ihr ihl =>
+        simp [zrcali]
+        rw [ihr, ihl]
+        constructor
+        · rfl
+        · rfl
 
 -- 2.c. Zbiranje elementov drevesa
 def zberi {A : Type} : tree A → List A :=
   fun t =>
-    sorry
+    match t with
+    | .empty => []
+    | .node x l r => stakni (zberi l) (stakni [x] (zberi r))
 
 -- Trditvi
 theorem trd16 {A : Type} {y : A} {xs ys : List A} : stakni xs (y::ys) = stakni (stakni xs [y]) ys :=
   by
-    sorry
+    induction xs with
+    | nil =>
+      simp [stakni]
+    | cons x xs' ih =>
+      simp [stakni]
+      rw [ih]
+
 
 theorem trd17 {A : Type} {t : tree A} : zberi (zrcali t) = obrni (zberi t) :=
   by
-    sorry
+    induction t with
+    | empty =>
+      simp [zrcali, zberi, obrni]
+    | node x l r ihr ihl =>
+      simp [zrcali, zberi, stakni]
+      rw [ihr, ihl]
+      rw [trd16]
+      simp [trd5, obrni]
+
 
 -- 2.d. Število elementov drevesa
 def velikost {A : Type} : tree A → Nat :=
   fun t =>
-    sorry
+    match t with
+    | .empty => 0
+    | .node _ r l => 1 + velikost r + velikost l
 
 theorem trd18 {A : Type} {t : tree A} : velikost (zrcali t) = velikost t :=
   by
-    sorry
+    induction t with
+    | empty =>
+        simp [zrcali]
+    | node x l r ihl ihr =>
+        simp [zrcali, velikost]
+        rw [ihl, ihr]
+        omega
 
 -- 3. Indukcija na pomožnih funkcijah z akumulatorjem - Seznami
+
+-- def obrni {A : Type} : List A → List A :=
+--   fun xs =>
+--     match xs with
+--     | [] => []
+--     | x :: xs' => stakni (obrni xs') [x]
+
 
 -- Definirajte repno rekurzivno funkcijo, ki obrne seznam
 def obrni' {A : Type} : List A → List A :=
@@ -199,6 +330,29 @@ def obrni' {A : Type} : List A → List A :=
     aux xs []
 
 -- Dokažite, da je vaša funkcija pravilna
+
+theorem pomozna_obrni {A : Type} : ∀ {xs acc : List A}, obrni'.aux xs acc = stakni (obrni xs) acc :=
+  by
+    intro xs acc
+    induction xs generalizing acc with
+    | nil =>
+        simp [obrni, stakni, obrni'.aux]
+    | cons x xs' ih =>
+        simp [obrni'.aux, obrni]
+        rw [ih]
+        simp [trd4, stakni]
+
+
 theorem obrni_enako_obrni' {A : Type} : ∀ {xs : List A}, obrni xs = obrni' xs :=
   by
-    sorry
+    intro xs
+    -- induction xs with
+    -- | nil =>
+    --     simp [obrni, obrni', obrni'.aux]
+    -- | cons x xs ih =>
+    --     simp [obrni, obrni', pomozna_obrni, trd3]
+    calc
+      obrni xs
+      _ = stakni (obrni xs) [] := by simp [trd3]
+      _ = obrni'.aux xs [] := by simp [pomozna_obrni]
+      _ = obrni' xs := by simp [obrni']
---
+++ domace-naloge/dn-3.lean
@@ -0,0 +1,397 @@
+set_option autoImplicit false
+
+/------------------------------------------------------------------------------
+ ## Naravna števila
+
+ - Definirajte funkcijo, ki *rekurzivno* (torej naivno in ne direktno s formulo,
+    ki jo boste morali dokazati) sešteje prvih `n` lihih naravnih števil, ter
+    dokažite, da zanjo velja znana enakost.
+ - Definirajte funkcijo, ki *rekurzivno* sešteje prvih `n` produktov dveh
+    zaporednih naravnih števil, ter dokažite da zanjo velja znana enakost
+    (najprej v obliki, ki ne zahteva deljenja, nato pa še v običajni obliki).
+
+ Indukcijska koraka dokazov trditev `vsota_lihih_kvadrat` in `formula_produktov`
+ dokažite z uporabo `calc` bloka.
+
+ *Namig*: pri krajših manipulacijah numeričnih literalov nam taktika `rfl`
+ pogosto zadostuje.
+------------------------------------------------------------------------------/
+
+-- Vsota prvih n lihih naravnih števil
+def vsota_lihih : Nat → Nat :=
+  fun n =>
+    match n with
+    | Nat.zero => Nat.zero
+    | Nat.succ m => vsota_lihih m + (2 * m + 1)
+
+theorem vsota_lihih_kvadrat : (n : Nat) → vsota_lihih n = n * n :=
+  by
+    intro n
+    induction n with
+    | zero =>
+        simp [vsota_lihih]
+    | succ m ih =>
+        calc
+          vsota_lihih (m + 1)
+          _ = vsota_lihih m + (2 * m + 1) := by simp [vsota_lihih]
+          _ = m * m + (2 * m + 1) := by rw [ih]
+          _ = m * m + (m + m + 1) := by rw [Nat.two_mul]
+          _ = m * m + m + m + 1 := by repeat rw [Nat.add_assoc]
+          _ = m * m + m * 1 + 1 * m + 1 * 1 := by rw [Nat.one_mul, Nat.mul_one]
+          _ = m * (m + 1) + 1 * m + 1 * 1 := by rw [← Nat.mul_add]
+          _ = m * (m + 1) + 1 * (m + 1) := by rw [Nat.add_assoc, ← Nat.mul_add]
+          _ = (m + 1) * (m + 1) := by rw [← Nat.add_mul]
+
+
+-- Vsota prvih n produktov zaporednih naravnih števil
+def vsota_produktov : Nat → Nat :=
+  fun n =>
+    match n with
+    | Nat.zero => Nat.zero
+    | Nat.succ m => vsota_produktov m + (m + 1) * (m + 2)
+
+theorem formula_produktov : (n : Nat) → 3 * vsota_produktov n = n * (n + 1) * (n + 2) :=
+  by
+    intro n
+    induction n with
+    | zero =>
+        simp [vsota_produktov]
+    | succ m ih =>
+        calc
+          3 * vsota_produktov (m + 1)
+          _ = 3 * (vsota_produktov m + (m + 1) * (m + 2)) := by rw [vsota_produktov]
+          _ = 3 * vsota_produktov m + 3 * (m + 1) * (m + 2) := by rw [Nat.mul_add, Nat.mul_assoc]
+          _ = m * (m + 1) * (m + 2) + 3 * (m + 1) * (m + 2) := by rw [ih]
+          _ = (m + 1) * (m + 2) * m + 3 * (m + 1) * (m + 2) := by rw [Nat.mul_comm m (m+1), Nat.mul_assoc, Nat.mul_comm m (m+2), ← Nat.mul_assoc]
+          _ = ((m + 1) * (m + 2)) * m + ((m + 1) * (m + 2)) * 3 := by rw [Nat.mul_comm 3 (m+1), Nat.mul_assoc (m+1) 3 (m+2), Nat.mul_comm 3 (m+2), ← Nat.mul_assoc]
+          _ = ((m + 1) * (m + 2)) * (m + 3) := by rw [← Nat.mul_add]
+          _ = (m + 1) * (m + 1 + 1) * (m + 1 + 2) := by rfl
+
+theorem prava_formula_produktov : (n : Nat) → vsota_produktov n = (n * (n + 1) * (n + 2)) / 3 :=
+  by
+    intro n
+    calc
+      vsota_produktov n
+      _ = vsota_produktov n * 3 / 3 := by omega
+      _ = 3 * vsota_produktov n / 3 := by rw [Nat.mul_comm]
+      _ = (n * (n + 1) * (n + 2)) / 3 := by simp [formula_produktov]
+
+/------------------------------------------------------------------------------
+ ## Vektorji
+
+ Definirajmo vektorje z elementi poljubnega tipa. Za ustrezno delovanje
+ zapišemo funkcijo stikanja dveh vektorjev s pomočjo taktik.
+
+ Zapišite še funkcije:
+ - `obrni`, ki vrne na vektor z elementi v obratnem vrstnem redu,
+ - `preslikaj`, ki preslika vse elemente vektorja z dano funkcijo,
+ - `zip`, ki združi dva vektorja v vektor parov,
+ - `dolzina`, ki vrne dolžino vektorja,
+ - `glava` in `rep`, ki varno vrneta glavo in rep *nepraznega* vektorja.
+ Rezultati operacij na testnem vektorju `[1,2,3]` so zapisani ob koncu
+ razdelka `Vektorji`.
+
+ Dokažite tudi trditve o teh funkcijah:
+ - `preslikaj_identiteto`: preslikava elementov vektorja z identiteto pusti
+    vektor nespremenjen,
+ - `preslikaj_kompozitum`: preslikava s kompozitumom funkcij je enaka
+    kompozitumu preslikav s posameznimi funkcijami,
+ - `dolzina_pravilna`: dolžina vektorja je enaka njegovi indeksirani dolžini,
+ - `preslikaj_in_zip_se_ujemata`: preslikava elementov vektorja in nato
+    združevanje z `zip` je enako združevanju z `zip` in nato preslikavi parov.
+------------------------------------------------------------------------------/
+
+inductive Vektor : Type → Nat → Type where
+  | prazen : {A : Type} → Vektor A 0
+  | sestavljen : {A : Type} → {n : Nat} → A → Vektor A n → Vektor A (n + 1)
+deriving Repr
+
+def stakni : {A : Type} → {m n : Nat} → Vektor A m → Vektor A n → Vektor A (m + n) :=
+  fun xs ys => match xs with
+  | .prazen =>
+    by
+      rw [Nat.add_comm]
+      exact ys
+  | .sestavljen x xs' =>
+    by
+      rw [Nat.add_right_comm]
+      exact .sestavljen x (stakni xs' ys)
+
+def obrni : {A : Type} → {m : Nat} → Vektor A m → Vektor A m :=
+  fun xs => match xs with
+  | .prazen => .prazen
+  | .sestavljen x xs' => stakni (obrni xs') (.sestavljen x .prazen)
+
+
+def preslikaj : {A B : Type} → {n : Nat} → (A → B) → Vektor A n → Vektor B n :=
+  fun f xs => match xs with
+  | .prazen => .prazen
+  | .sestavljen x xs' => .sestavljen (f x) (preslikaj f xs')
+
+
+def zip : {A B : Type} → {n : Nat} → Vektor A n → Vektor B n → Vektor (A × B) n :=
+  fun xs ys => match xs with
+  | .prazen => .prazen
+  | .sestavljen x xs' => match ys with
+                        | .sestavljen y ys' => .sestavljen (x, y) (zip xs' ys')
+
+def dolzina : {A : Type} → {n : Nat} → Vektor A n → Nat :=
+  fun xs => match xs with
+  | .prazen => 0
+  | .sestavljen _ xs' => 1 + dolzina xs'
+
+def glava : {A : Type} → {n : Nat} → Vektor A (n+1) → A :=
+  fun xs => match xs with
+  | .sestavljen x _ => x
+
+def rep {A : Type} {n : Nat} : Vektor A (n+1) → Vektor A n :=
+  fun xs => match xs with
+  | .sestavljen _ xs' => xs'
+
+theorem preslikaj_identiteto : {A : Type} → {n : Nat} → (xs : Vektor A n) →
+  preslikaj id xs = xs :=
+  by
+    intro A n xs
+    induction xs with
+    | prazen =>
+      simp [preslikaj]
+    | sestavljen x xs' ih =>
+      simp [preslikaj, ih]
+
+theorem preslikaj_kompozitum :
+  {A B C : Type} → {n : Nat} → (f : A → B) → (g : B → C) → (xs : Vektor A n) →
+  preslikaj (fun x => g (f x)) xs = preslikaj g (preslikaj f xs) :=
+  by
+    intro A B C n f g xs
+    induction xs with
+    | prazen =>
+      simp [preslikaj]
+    | sestavljen x xs' ih =>
+      simp [preslikaj, ih]
+
+theorem dolzina_pravilna : {A : Type} → {n : Nat} → (xs : Vektor A n) →
+  dolzina xs = n :=
+  by
+    intro A n xs
+    induction xs with
+    | prazen =>
+      simp [dolzina]
+    | sestavljen x xs' ih =>
+      simp [dolzina, ih]
+      omega
+
+theorem preslikaj_in_zip_se_ujemata : {A B C : Type} → {n : Nat} →
+  (f : A → B) → (g : A → C) → (xs : Vektor A n) →
+  zip (preslikaj f xs) (preslikaj g xs) = preslikaj (fun x => (f x, g x)) xs :=
+  by
+    intro A B C n f g xs
+    induction xs with
+    | prazen =>
+      simp [preslikaj, zip]
+    | sestavljen x xs' ih =>
+      simp [preslikaj, zip, ih]
+
+-- Primeri rezultatov operacij
+def primer_vektorja : Vektor Nat 3 :=
+  .sestavljen 1 (.sestavljen 2 (.sestavljen 3 .prazen))
+
+#eval obrni primer_vektorja
+-- Vrne: Vektor.sestavljen 3 (Vektor.sestavljen 2 (Vektor.sestavljen 1 (Vektor.prazen)))
+#eval preslikaj (fun x => x + 10) primer_vektorja
+-- Vrne: Vektor.sestavljen 11 (Vektor.sestavljen 12 (Vektor.sestavljen 13 (Vektor.prazen)))
+#eval zip primer_vektorja primer_vektorja
+-- Vrne: Vektor.sestavljen (1, 1) (Vektor.sestavljen (2, 2) (Vektor.sestavljen (3, 3) (Vektor.prazen)))
+#eval dolzina primer_vektorja
+-- Vrne: 3
+#eval glava primer_vektorja
+-- Vrne: 1
+#eval rep primer_vektorja
+-- Vrne: Vektor.sestavljen 2 (Vektor.sestavljen 3 (Vektor.prazen))
+
+/------------------------------------------------------------------------------
+ ## Logične izjave in predikatni račun
+
+ Dokažite spodnje logične trditve.
+
+ Dokazati morate 3 izjave brez predikatov in 3 izjave s predikatoma `forall`
+ in `exists`. Zadnja je _paradoks pivca_, ki pravi:
+   "V vsaki neprazni gostilni obstaja gost, za katerega velja,
+   da če pije on, pijejo vsi v gostilni."
+
+ Pri nekaterih dokazih boste potrebovali dokaze klasične logike iz modula
+ `Classical`.
+------------------------------------------------------------------------------/
+
+theorem dvojna_negacija {P : Prop} : ¬¬P ↔ P :=
+  by
+    apply Iff.intro
+    · intro h
+      apply Classical.byContradiction
+      intro h1
+      contradiction
+    · intro h
+      intro h1
+      contradiction
+
+
+theorem trojna_negacija {P : Prop} : ¬¬¬P ↔ ¬P :=
+  by
+    apply Iff.intro
+    · intro h
+      apply Classical.byContradiction
+      intro h1
+      contradiction
+    · intro h
+      intro h1
+      contradiction
+
+theorem kontrapozitivna_oblika {P Q : Prop} : (P → Q) ↔ (¬Q → ¬P) :=
+  by
+    apply Iff.intro
+    · intro hpq
+      intro hnq
+      intro hp
+      exact hnq (hpq hp)
+    · intro hnqnp
+      intro hp
+      apply Classical.byContradiction
+      intro hnq
+      have hnp : ¬P := hnqnp hnq
+      contradiction
+
+
+theorem pravilo_obstaja_disjunkcija : {A : Type} → {P Q : A → Prop} →
+  (∃ x, P x ∨ Q x) ↔ (∃ x, P x) ∨ (∃ x, Q x) :=
+  by
+    intro A P Q
+    apply Iff.intro
+    · intro h
+      rcases h with ⟨x, hx⟩
+      cases hx with
+      | inl hp =>
+        left
+        exact ⟨x, hp⟩
+      | inr hq =>
+        right
+        exact ⟨x, hq⟩
+    · intro h
+      cases h with
+      | inl hpx =>
+        rcases hpx with ⟨x, hP⟩
+        exact ⟨x, Or.inl hP⟩
+      | inr hqx =>
+        rcases hqx with ⟨x, hQ⟩
+        exact ⟨x, Or.inr hQ⟩
+
+
+theorem obstaja_p_ali_za_vse_ne_p {A : Type} {P : A → Prop} :
+  (∃ x, P x) ∨ (∀ x, ¬ P x) :=
+  by
+    by_cases h : ∃ x, P x
+    · left
+      exact h
+    · right
+      intro x
+      intro hpx
+      have : ∃ x, P x := ⟨x, hpx⟩
+      exact h this
+
+theorem paradoks_pivca :
+  {G : Type} → {P : G → Prop} →
+  (g : G) →  -- (g : G) pove, da je v gostilni vsaj en gost
+  ∃ (p : G), (P p → ∀ (x : G), P x) :=
+  by
+    sorry
+
+/------------------------------------------------------------------------------
+ ## Dvojiška drevesa
+
+  Podan je tip dvojiških dreves skupaj s funkcijami za zrcaljenje drevesa,
+  izračun višine in velikosti drevesa.
+  Dokažite trditvi:
+ - `zrcaljenje_ohrani_visino`: zrcaljenje drevesa ohrani njegovo višino,
+ - `visina_manjsa_ali_enaka_velikosti`: višina drevesa je vedno manjša ali
+    enaka njegovi velikosti.
+
+  V drugem delu sta definirani funkciji `vsota` in `vsota'`, ki izračunata
+  vsoto vseh elementov v drevesu z naravnimi števili. Prva jo izračuna naivno,
+  druga pa z uporabo pomožne funkcije z akumulatorjem. Dokažite, da obe funkciji
+  dajeta enak rezultat za vsako drevo z naravnimi števili.
+  Do pomožne funkcije `aux` lahko dostopate kot `vsota'.aux`.
+-------------------------------------------------------------------------------/
+
+inductive Drevo : Type → Type where
+  | prazno : {A : Type} → Drevo A
+  | sestavljeno : {A : Type} → A → Drevo A → Drevo A → Drevo A
+
+def visina : {A : Type} → Drevo A → Nat :=
+  fun t => match t with
+  | .prazno => 0
+  | .sestavljeno _ l d => 1 + max (visina l) (visina d)
+
+def zrcali : {A : Type} → Drevo A → Drevo A :=
+  fun t => match t with
+  | .prazno => .prazno
+  | .sestavljeno x l d => .sestavljeno x (zrcali d) (zrcali l)
+
+def velikost : {A : Type} → Drevo A → Nat :=
+  fun t => match t with
+  | .prazno => 0
+  | .sestavljeno _ l d => 1 + velikost l + velikost d
+
+theorem zrcaljenje_ohrani_visino :
+  {A : Type} → (t : Drevo A) →
+  visina (zrcali t) = visina t :=
+  by
+    intro A t
+    induction t with
+    | prazno =>
+      simp [zrcali]
+    | sestavljeno x l d ihl ihd =>
+      simp [zrcali, visina, ihl, ihd, Nat.max_comm]
+
+theorem visina_manjsa_ali_enaka_velikosti :
+  {A : Type} → (t : Drevo A) →
+  visina t ≤ velikost t :=
+  by
+    intro A t
+    induction t with
+    | prazno =>
+      simp [visina]
+    | sestavljeno x l d ihl ihd =>
+      simp [visina, velikost]
+      omega
+
+-- Drugi del
+def vsota : Drevo Nat → Nat :=
+  fun t => match t with
+  | .prazno => 0
+  | .sestavljeno x l d => x + vsota l + vsota d
+
+def vsota' : Drevo Nat → Nat :=
+  let rec aux : Drevo Nat → Nat → Nat :=
+    fun t acc => match t with
+    | .prazno => acc
+    | .sestavljeno x l d => aux l (x + aux d acc)
+  fun t => aux t 0
+
+theorem pomozna : ∀ t acc, vsota'.aux t acc = vsota t + acc :=
+  by
+    intro t
+    induction t with
+    | prazno =>
+      intro acc
+      simp [vsota, vsota'.aux]
+    | sestavljeno x l d ihl ihd =>
+      intro acc
+      simp [vsota'.aux, vsota]
+      rw [ihl]
+      rw [ihd]
+      omega
+
+
+
+theorem vsota_eq_vsota' : ∀ {t : Drevo Nat}, vsota t = vsota' t :=
+  by
+    intro t
+    simp [vsota', pomozna]
